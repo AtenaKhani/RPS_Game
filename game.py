e3cf0b1731ebf90f636a640555e450af88809dae
Atena <atenakhani904@gmail.com>
2024-08-06 06:57:55 +0330

    update import statements

--- game.py
+++ game.py
@@ -1,8 +1,7 @@
 from tabulate import tabulate
-from sqlalchemy import create_engine, Column, Integer, String, desc, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
 Base = declarative_base()
 

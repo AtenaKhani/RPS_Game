7a5d5c7f00e1af1ee10d225092bd3401cfc57dc8
Atena <atenakhani904@gmail.com>
2024-08-17 21:09:12 +0330

    Add: Implement Server class for Rock-Paper-Scissors game with socket communication

---
+++ server.py
@@ -0,0 +1,12 @@
+import socket
+import threading
+
+
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clients = []
+        self.player_info = []
+

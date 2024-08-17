f2871c5ed200adc53e3b9345acf5c1611131955b
Atena <atenakhani904@gmail.com>
2024-08-17 21:17:01 +0330

    Add: Implement handle_client method to manage client connections

--- server.py
+++ server.py
@@ -20,3 +20,18 @@ class Server:
             task = threading.Thread(target=self.handle_client, args=(client_socket,), daemon=True)
             task.start()
 
+    def handle_client(self, client_socket):
+        self.clients.append(client_socket)
+        client_socket.send("Hi, welcome to the game. Please enter your name: ".encode())
+        while True:
+            name = client_socket.recv(1024).decode().strip()
+            if name in self.names:
+                client_socket.send("This name is already taken. Please choose a different name.".encode())
+            else:
+                self.player_info[name] = 0
+                break
+        if len(self.names) == 2:
+            for client_socket in self.clients:
+                client_socket.send("Game is starting!".encode())
+            self.play_game()
+

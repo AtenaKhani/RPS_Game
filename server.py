72addc04ab911201329f1c8d5e911e33fc2f3b34
Atena <atenakhani904@gmail.com>
2024-08-17 21:13:51 +0330

    Add: Implement start method to initialize server and handle clients in separate threads

--- server.py
+++ server.py
@@ -10,3 +10,13 @@ class Server:
         self.clients = []
         self.player_info = []
 
+    def start(self):
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(2)
+        print(f" Listening on {self.host}:{self.port}")
+        while True:
+            client_socket, addr = self.server_socket.accept()
+            print(f" Accepted connection from {addr}")
+            task = threading.Thread(target=self.handle_client, args=(client_socket,), daemon=True)
+            task.start()
+
